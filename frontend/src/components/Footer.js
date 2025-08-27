d492521f026e88c966e2fed953654c01f597b19a
Aziz boukhchim <azizboukhchim321@gmail.com>
2025-08-27 14:05:55 +0200

    added redirect

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/Footer.css";
 import { Link } from "react-router-dom";
-import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF, FaInstagram} from "react-icons/fa";
 
 const Footer = () => {
   return (
